b65e06362eec0ba46c2b4d75893e13ce7622d057
ashutoshshrinet0129@gmail.com <ashutoshshrinet0129@gmail.com>
2026-09-21 17:02:40 +0530

    Day - 3
    Day - 3

---
+++ addSymbolPattern.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+int i , j , n;
+printf(" Enter Side (Odd Number) : \n ");
+scanf("%d",&n);
+int mid = n/2+1;
+for ( i = 1; i <= n ; i++)
+{
+    for ( j = 1; j <=n; j++)
+    {
+       if (i == mid || j == mid )
+       {
+printf("* ");
+       }
+       else{
+        printf("  ");
+
+       }
+    }
+    printf("\n");
+
+}
+
+     return 0;
+}
